59c6ba8c1dbfa3fd75a81478ab4ddb12f2e33fc6
Carl Mitchell <cmitchell@physics.rutgers.edu>
2016-04-29 16:33:52 -0400

    +pep8 setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name = 'saltfppipe',
-      version = '0.0.1',
-      description = 'SALT Fabry-Perot Data Reduction Software',
-      author = 'Carl J. Mitchell',
-      author_email = 'cmitchell@physics.rutgers.edu',
-      packages = find_packages()
+setup(name='saltfppipe',
+      version='0.0.1',
+      description='SALT Fabry-Perot Data Reduction Software',
+      author='Carl J. Mitchell',
+      author_email='cmitchell@physics.rutgers.edu',
+      packages=find_packages()
       )
